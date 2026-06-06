e77f4e7586afa448fe5233098905db1280ffeba8
CinthiaTenesaca <cinthia2003maria@gmail.com>
2026-06-06 18:12:13 -0500

    fix - modificacion para funcionamiento con bd

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+
+  supabaseUrl:
+    'https://rjccxgdmkkljqnywellt.supabase.co',
+
+  supabaseKey:
+    'sb_publishable_zHqFevmlc9rbdL4aGTK-Ag_oks0TpGc'
+};
